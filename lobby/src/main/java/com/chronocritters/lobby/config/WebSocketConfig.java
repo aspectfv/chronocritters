1d74cd7e37e55ee6618c483ab18d272658f6e066
aspectzxcc <jos10nathan@gmail.com>
2025-09-01 10:52:34 +0800

    added non nulls to websocket config

--- lobby/src/main/java/com/chronocritters/lobby/config/WebSocketConfig.java
+++ lobby/src/main/java/com/chronocritters/lobby/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.chronocritters.lobby.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,7 +12,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         // The endpoint clients will connect to: ws://<host>/ws
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
@@ -19,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
     // Prefixes for messages sent from server to client (subscriptions)
     config.enableSimpleBroker("/topic", "/user"); // /user for unicast, /topic for broadcast
     // Prefix for messages sent from client to server (app destination)
