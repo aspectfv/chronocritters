7f94c26961eee53607d1d40b34b6be45538a4221
aspectzxcc <jos10nathan@gmail.com>
2025-09-02 00:33:01 +0800

    added gamelogic module

---
+++ gamelogic/src/main/java/com/chronocritters/gamelogic/GamelogicApplication.java
@@ -0,0 +1,13 @@
+package com.chronocritters.gamelogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelogicApplication.class, args);
+	}
+
+}
---
+++ gamelogic/src/test/java/com/chronocritters/gamelogic/GamelogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronocritters.gamelogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
