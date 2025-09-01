b2b4381b94cb279b2887c74ee69c9281deca82a5
aspectzxcc <jos10nathan@gmail.com>
2025-09-02 00:25:02 +0800

    import fix

--- user/src/main/java/com/chronocritters/user/controller/AuthController.java
+++ user/src/main/java/com/chronocritters/user/controller/AuthController.java
@@ -1,16 +1,14 @@
 package com.chronocritters.user.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import com.chronocritters.user.service.AuthService;
-
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.chronocritters.user.dto.LoginRequest;
 import com.chronocritters.user.dto.RegisterRequest;
+import com.chronocritters.user.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
 
 
 @RestController
