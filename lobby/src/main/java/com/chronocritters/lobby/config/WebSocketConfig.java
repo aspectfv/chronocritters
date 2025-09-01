1c995db19f427ab9cbea334720664f368ea45605
aspectzxcc <jos10nathan@gmail.com>
2025-09-01 10:08:32 +0800

    added websocket config

---
+++ lobby/src/main/java/com/chronocritters/lobby/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.chronocritters.lobby.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // The endpoint clients will connect to: ws://<host>/ws
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // sockjs fallback for no websocket support
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+    // Prefixes for messages sent from server to client (subscriptions)
+    config.enableSimpleBroker("/topic", "/user"); // /user for unicast, /topic for broadcast
+    // Prefix for messages sent from client to server (app destination)
+    config.setApplicationDestinationPrefixes("/app");
+    // Prefix for user-specific messages (unicast)
+    config.setUserDestinationPrefix("/user");
+    }
+}
