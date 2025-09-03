c1a6740a179619d2a6d37caef57ab273d63e73a8
aspectzxcc <josh10nathan@gmail.com>
2025-09-03 21:31:06 +0800

    added env

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -3,11 +3,11 @@ import { applyAuthTokenInterceptor } from '@api/interceptors';
 import type { LoginCredentials, RegisterCredentials } from '@features/auth/types';
 
 export const userClient = axios.create({
-  baseURL: import.meta.env.VITE_USER_SERVICE_URL || 'http://localhost:8081', // User service port
+  baseURL: import.meta.env.VITE_USER_SERVICE_URL || 'http://localhost:8080', // User service port
 });
 
 // Add the auth token logic to this client
 applyAuthTokenInterceptor(userClient);
 
-export const login = (credentials: LoginCredentials) => userClient.post('/login', credentials);
-export const register = (credentials: RegisterCredentials) => userClient.post('/register', credentials);
+export const login = (credentials: LoginCredentials) => userClient.post('/auth/login', credentials);
+export const register = (credentials: RegisterCredentials) => userClient.post('/auth/register', credentials);
