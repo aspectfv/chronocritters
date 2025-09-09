42bda159518d48a037a4f5feba5d5f7fbf9230b0
aspectzxcc <josh10nathan@gmail.com>
2025-09-09 17:49:42 +0800

    changed to rest exception handler for gamelogic service

--- gamelogic/src/main/java/com/chronocritters/gamelogic/config/GameLogicSecurityConfig.java
+++ gamelogic/src/main/java/com/chronocritters/gamelogic/config/GameLogicSecurityConfig.java
@@ -3,10 +3,10 @@ package com.chronocritters.gamelogic.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
-import com.chronocritters.lib.config.BaseReactiveSecurityConfig;
+import com.chronocritters.lib.exception.BaseRestExceptionHandler;
 
 @Configuration
 @EnableWebFluxSecurity
-public class GameLogicSecurityConfig extends BaseReactiveSecurityConfig {
+public class GameLogicSecurityConfig extends BaseRestExceptionHandler {
     // Inherits reactive security configuration
 }
--- gamelogic/src/main/java/com/chronocritters/gamelogic/exception/GameLogicExceptionHandler.java
+++ gamelogic/src/main/java/com/chronocritters/gamelogic/exception/GameLogicExceptionHandler.java
@@ -1,10 +1,10 @@
 package com.chronocritters.gamelogic.exception;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import com.chronocritters.lib.exception.BaseReactiveExceptionHandler;
+import com.chronocritters.lib.exception.BaseRestExceptionHandler;
 
 @RestControllerAdvice
-public class GameLogicExceptionHandler extends BaseReactiveExceptionHandler {
-    // Inherits all exception handling methods from BaseReactiveExceptionHandler
+public class GameLogicExceptionHandler extends BaseRestExceptionHandler {
+    // Inherits all exception handling methods from BaseRestExceptionHandler
     // Can add service-specific exception handlers here if needed
 }
