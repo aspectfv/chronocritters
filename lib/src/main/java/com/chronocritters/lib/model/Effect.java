d255685cd72a98bd39386793f9ab674405bd47a8
aspectzxcc <josh10nathan@gmail.com>
2025-09-11 17:31:37 +0800

    added json subtypes for effect's json serialization

--- lib/src/main/java/com/chronocritters/lib/model/Effect.java
+++ lib/src/main/java/com/chronocritters/lib/model/Effect.java
@@ -3,6 +3,11 @@ package com.chronocritters.lib.model;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.chronocritters.lib.context.EffectContext;
+import com.chronocritters.lib.model.effects.DamageEffect;
+import com.chronocritters.lib.model.effects.DamageOverTimeEffect;
+import com.chronocritters.lib.model.effects.SkipTurnEffect;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +19,16 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 @Document(collection = "effects")
+@JsonTypeInfo(
+  use = JsonTypeInfo.Id.NAME, 
+  include = JsonTypeInfo.As.PROPERTY, 
+  property = "type"
+)
+@JsonSubTypes({ 
+  @JsonSubTypes.Type(value = DamageEffect.class, name = "DAMAGE"), 
+  @JsonSubTypes.Type(value = DamageOverTimeEffect.class, name = "DAMAGE_OVER_TIME"),
+  @JsonSubTypes.Type(value = SkipTurnEffect.class, name = "SKIP_TURN")
+})
 public abstract class Effect {
     protected String id;
     protected EffectType type;
